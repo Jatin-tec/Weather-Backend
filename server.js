f73f77de59bc6ced17c0b5f0f1a929d591e7f397
jatin <jatin.kshatriya2821@gmail.com>
2022-11-24 00:52:29 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+var express = require('express');
+var app = express();
+var cors = require('cors')
+
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
+
+const api_key = '22a23f13ac0c88b42ec96a8ec9bcddcc';
+
+app.use(cors());
+app.get('/get-wether', async function (req, res) {
+
+    const resolveRequests = (body, key) => {
+
+        let part = [];
+        let bodyArray = [];
+
+        for (var prop in body) {
+            bodyArray.push(body[prop]);
+        }
+
+        part = bodyArray.slice(key*10, (key*10)+10);
+        
+        const querry = [];
+        part.forEach((val) => {
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${val.latitude}&lon=${val.longitude}&appid=${api_key}`;
+            const result = axios.get(url);
+            querry.push(result);
+        })
+
+        return Promise.all(querry);
+    }
+
+    const key = req.query.key;
+    const data = req.query.data;
+
+    if (myCache.get(`data${key}`) === undefined) {
+
+        const result = await resolveRequests(data, key);
+
+        const resultArray=[];
+        result.forEach((doc) => {
+            resultArray.push(doc.data);
+        })
+
+        if (resultArray !== []){
+            myCache.set(`data${key}`, resultArray, 300)
+        }
+
+    }
+    
+    res.json(myCache.get(`data${key}`));
+})
+
+app.listen(3000);
